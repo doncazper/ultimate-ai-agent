1636e02f37c7ad3c27f39f4b3195e12763018bb1
doncazper <caztronics@yahoo.com>
2026-06-24 10:05:16 -0700

    Export WebAccessGateway package API

---
+++ src/ultimate_ai_agent/core/web_access/__init__.py
@@ -0,0 +1,34 @@
+"""Policy-controlled WebAccessGateway package."""
+
+from .contracts import (
+    SourceMetadata,
+    WebAccessAdapterKind,
+    WebAccessAuditRecord,
+    WebAccessAuthorityMode,
+    WebAccessNetworkLane,
+    WebAccessPolicyDecision,
+    WebAccessPolicyStatus,
+    WebAccessRequest,
+    WebAccessRequestKind,
+    WebAccessResult,
+    WebAccessRiskClass,
+)
+from .gateway import WebAccessGateway, create_default_web_access_gateway
+from .policy import WebAccessPolicy
+
+__all__ = [
+    "SourceMetadata",
+    "WebAccessAdapterKind",
+    "WebAccessAuditRecord",
+    "WebAccessAuthorityMode",
+    "WebAccessGateway",
+    "WebAccessNetworkLane",
+    "WebAccessPolicy",
+    "WebAccessPolicyDecision",
+    "WebAccessPolicyStatus",
+    "WebAccessRequest",
+    "WebAccessRequestKind",
+    "WebAccessResult",
+    "WebAccessRiskClass",
+    "create_default_web_access_gateway",
+]
